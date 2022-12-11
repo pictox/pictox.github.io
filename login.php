6c3ff9cc65f14f2bf80efe61a842ecf16541088e
Webara <vdarklegendff@gmail.com>
2022-12-11 13:32:52 +0530

    pictox web

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PicTox Login</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<center>
+
+<h1>Login To PicTox For Continue
+</h1>
+<form action="login process.php" method="POST">
+
+<p>Login</p>
+    <input type="text" id="user" name="username" placeholder="Username"/><br>
+    <input type="text" id="pass" name="password" placeholder="Password"/><br>
+    <button type="submit" id="btn" name="login" default>Login</button>
+
+</form>
+
+<button id="btn" name="login">Create a New Account</button>
+</center>
+
+</body>
+</html>
